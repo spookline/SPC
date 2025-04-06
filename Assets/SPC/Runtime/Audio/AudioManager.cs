75bf97688210296e6f576bf6bd105df8eaa899ec
ReaperMaga <reapermaga@gmail.com>
2025-04-06 02:00:16 +0200

    chore: Audio

--- Assets/SPC/Runtime/Audio/AudioManager.cs
+++ Assets/SPC/Runtime/Audio/AudioManager.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Pool;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace Spookline.SPC.Audio {
     /// <summary>
@@ -12,87 +15,93 @@ namespace Spookline.SPC.Audio {
     public class AudioManager : Singleton<AudioManager> {
 
         private readonly Dictionary<string, AudioClip> _clips = new();
-        private readonly Dictionary<GameObject, AudioPlayer> _players = new();
-
-        private GameObject FallbackTarget => _fallbackTarget ??= new GameObject("AudioMainContainer");
-        private GameObject _fallbackTarget;
-
+        private readonly ObjectPool<AudioTrackedObject> _pool;
+        
         public AudioManager() {
             if (IsInitialized) return;
-            // Cleanup
-            SceneManager.sceneUnloaded += _ => {
-                var keys = new List<GameObject>(_players.Keys);
-                foreach (var key in keys.Where(key => key == null)) {
-                    _players.Remove(key);
-                }
-            };
+            _pool = new ObjectPool<AudioTrackedObject>(OnPoolCreate, OnPoolGet, OnPoolRelease, OnPoolDestroy);
         }
 
-        /// <summary>
-        /// Plays an audio clip based on the provided audio definition, optional target GameObject, and delay time.
-        /// </summary>
-        /// <param name="definition">The audio definition specifying the audio clip to play, along with its playback properties such as loop, volume, and pitch.</param>
-        /// <param name="targetObject">The target GameObject on which the audio clip should be played. Defaults to a fallback GameObject if not specified.</param>
-        /// <param name="delay">The time in seconds to delay the playback of the audio clip. Defaults to 0.0f for immediate playback.</param>
-        public void Play(AudioDefinition definition, GameObject targetObject = null, float delay = 0.0f) {
+        public void Release(AudioTrackedObject trackedObject) {
+            _pool.Release(trackedObject);
+        }
+
+        private static AudioTrackedObject OnPoolCreate()  {
+            var sourceObject = new GameObject("PooledAudioSource");
+            sourceObject.AddComponent<AudioSource>();
+            return sourceObject.AddComponent<AudioTrackedObject>();
+        }
+
+        private static void OnPoolGet(AudioTrackedObject trackedObject) {
+            trackedObject.enabled = true;
+            trackedObject.source.enabled = true;
+        }
+
+        private static void OnPoolRelease(AudioTrackedObject trackedObject) {
+            trackedObject.source.Stop();
+            trackedObject.source.enabled = false;
+            trackedObject.enabled = false;
+        }
+        
+        private static void OnPoolDestroy(AudioTrackedObject trackedObject) {
+            Object.Destroy(trackedObject.gameObject);
+        }
+
+        public void PlayAtTarget(AudioDefinition definition, GameObject target) {
+            
+        }
+        
+        public void Play(AudioDefinition definition) {
+            Play(definition, Vector3.zero);
+        }
+        
+        public void Play(AudioDefinition definition, Vector3 position) {
             var clip = GetClip(definition.audioAsset);
-            if (clip == null) return;
-            var target = targetObject ?? FallbackTarget;
-            var source = GetOrCreateSource(definition, target);
-            if (delay > 0.0f) {
-                source.Play();
-            } else {
-                source.PlayDelayed(delay);
-            }
-        }
-
-        /// <summary>
-        /// Stops the playback of an audio clip based on the provided audio definition and optional target GameObject.
-        /// </summary>
-        /// <param name="definition">The audio definition identifying the specific audio clip to stop.</param>
-        /// <param name="targetObject">The target GameObject from which the audio clip should be stopped. Defaults to a fallback GameObject if not provided.</param>
-        public void Stop(AudioDefinition definition, GameObject targetObject = null) {
-            var target = targetObject ?? FallbackTarget;
-            if (!_players.TryGetValue(target, out var player)) return;
-            if (player.sources.TryGetValue(definition, out var source)) {
-                source.Stop();
-            }
-        }
-
-        private AudioSource CreateSource(AudioDefinition definition, GameObject target) {
-            var audioSource = target.AddComponent<AudioSource>();
-            audioSource.clip = GetClip(definition.audioAsset);
-            audioSource.volume = definition.volume;
-            audioSource.pitch = definition.pitch;
-            audioSource.spatialBlend = definition.spatialBlend;
-            audioSource.minDistance = definition.minDistance;
-            audioSource.maxDistance = definition.maxDistance;
-            audioSource.loop = definition.loop;
-            audioSource.rolloffMode = AudioRolloffMode.Linear;
-            return audioSource;
-        }
-
-        private AudioSource GetOrCreateSource(AudioDefinition definition, GameObject target) {
-            if (!_players.ContainsKey(target)) {
-                _players[target] = new AudioPlayer();
-            }
-
-            var player = _players[target];
-            if (!player.sources.ContainsKey(definition)) {
-                player.sources[definition] = CreateSource(definition, target);
-            }
-
-            return player.sources[definition];
+            if(clip == null) return;
+            var trackedObject = _pool.Get();
+            trackedObject.source.clip = clip;
+            definition.Apply(trackedObject.source);
+            trackedObject.Play(position);
         }
 
         private AudioClip GetClip(string asset) {
             if (!_clips.ContainsKey(asset)) {
-                _clips[asset] =
-                    Addressables.LoadAssetAsync<AudioClip>(asset).WaitForCompletion();
+                _clips[asset] = Addressables.LoadAssetAsync<AudioClip>(asset).WaitForCompletion();
             }
-
             return _clips[asset];
         }
+
+    }
+
+    public class AudioTrackedObject : MonoBehaviour {
+
+        [HideInInspector]
+        public AudioSource source;
+        
+        private bool _started;
+
+        private void Awake() {
+            source = GetComponent<AudioSource>();
+        }
+
+        private void Update() {
+            if (_started && source.isPlaying) {
+                _started = false;
+                return;
+            }
+            if (!source.isPlaying && !_started) {
+                AudioManager.Instance.Release(this);
+            }
+        }
+
+
+
+        public void Play(Vector3 position) {
+            transform.position = position;
+            _started = true;
+            source.Play();
+        }
+        
 
     }
 
@@ -109,6 +118,15 @@ namespace Spookline.SPC.Audio {
         public AudioDefinition(string audioAsset) {
             this.audioAsset = audioAsset;
         }
+        public void Apply(AudioSource source) {
+            source.volume = volume;
+            source.pitch = pitch;
+            source.spatialBlend = spatialBlend;
+            source.minDistance = minDistance;
+            source.maxDistance = maxDistance;
+            source.loop = loop;
+        }
+        
 
     }
 
